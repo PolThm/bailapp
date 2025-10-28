2fedf254ef731e86fcd7fe6ab4c511844bd9a351
Pol Thomas <pol.thomas56@gmail.com>
2025-10-28 18:49:05 +0100

    Deplace images

--- apps/web/src/components/SplashScreen.tsx
+++ apps/web/src/components/SplashScreen.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import dancingCoupleLogo from '../../public/images/dancing-couple-transparent.png';
+import dancingCoupleLogo from '@/images/dancing-couple-transparent.png';
 
 interface SplashScreenProps {
   onFinish: () => void;
