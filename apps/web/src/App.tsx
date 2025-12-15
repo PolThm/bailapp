078700b9f21e36108a81532cd80105c72973a059
Pol Thomas <pol.thomas56@gmail.com>
2025-12-15 17:13:49 +0100

    feat: add a todo comment in App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -29,6 +29,8 @@ const queryClient = new QueryClient({
   },
 });
 
+// TODO: Allow users to update videos
+
 // Disable SplashScreen and PWA Install Prompt in development
 const isProduction = import.meta.env.PROD;
 
